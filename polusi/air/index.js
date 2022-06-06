a7bda619e47a8bc6a5237352568ba1401f45caa0
Your Name <111201911901@mhs.dinus.ac.id>
2022-06-06 13:17:16 +0700

    revisi 2 api

--- polusi/air/index.js
+++ polusi/air/index.js
@@ -1,19 +1,13 @@
-const {parse} = require('csv-parse');
+const parse = require('csv-parser');
 const fs = require('fs');
 
 const csvData = [];
 
 fs.createReadStream(__dirname + '/polusi_2010-2021.csv')
-    .pipe(
-        parse({
-            delimiter: ','
-        })
-    )
-    .on('data', function (dataRow) {
-        csvData.push(dataRow);
-    })
-    .on('end', function () {
-        console.log(csvData);
-    });
+.pipe(parse())
+.on('data', (data) => csvData.push(data))
+.on('end',() => {
+    console.log(csvData);
+})
 
     module.exports=csvData
